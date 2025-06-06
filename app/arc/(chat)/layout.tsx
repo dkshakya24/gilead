de2f5e533baa461ed9aa8c8b8aa79eee78717e46
dkotha-290614 <dilip.kotha@gilead.com>
2025-06-06 17:28:26 +0530

    Update layout.tsx

--- app/arc/(chat)/layout.tsx
+++ app/arc/(chat)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/header'
+import { Header } from '@/components/Header'
 import { SidebarDesktop } from '@/components/sidebar-desktop'
 import SourceMultiSelect from '@/components/source-multi-select'
 interface ChatLayoutProps {
