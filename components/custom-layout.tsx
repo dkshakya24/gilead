9f2c234e6b29cc40c847907922bea32d233e78f1
dkotha-290614 <dilip.kotha@gilead.com>
2025-06-06 17:28:54 +0530

    Update custom-layout.tsx

--- components/custom-layout.tsx
+++ components/custom-layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { Header } from '@/components/header'
+import { Header } from '@/components/Header'
 
 const CustomLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname()
