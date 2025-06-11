97464033afcb6b061dab554a17a434db63b0cd89
dkshakya24 <shakyadeep@Deepak-1171-M-B.local>
2025-06-11 19:38:41 +0530

    chanegs for UI in feedback

--- app/api/utils/generate-id/route.ts
+++ app/api/utils/generate-id/route.ts
@@ -4,7 +4,18 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   try {
     const uuid = uuidv4()
-    return NextResponse.json({ id: uuid }, { status: 200 })
+    return NextResponse.json(
+      { id: uuid },
+      {
+        status: 200,
+        headers: {
+          'Cache-Control':
+            'no-store, no-cache, must-revalidate, proxy-revalidate',
+          Pragma: 'no-cache',
+          Expires: '0'
+        }
+      }
+    )
   } catch (error) {
     return NextResponse.json(
       { error: 'Failed to generate UUID' },
