460269afce910826f04b86a6c31ac246f822e17e
dkshakya24 <shakyadeep@Deepak-1171-M-B.local>
2025-07-09 15:57:35 +0530

    chanegs for UI in feedback

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -143,7 +143,7 @@ export function SidebarItem({
         </span>
       </div>
       {isActive && (
-        <div className="absolute right-[8px] top-[1px]">{children}</div>
+        <div className="absolute right-[8px] top-[5px]">{children}</div>
       )}
     </motion.div>
   )
