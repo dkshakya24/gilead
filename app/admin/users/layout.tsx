4e0de0cc2ce6b22c677dbdd8a9d065000e09d2f2
dkshakya24 <shakyadeep@Deepak-1171-M-B.local>
2025-07-11 16:53:42 +0530

    chanegs for UI in feedback

--- app/admin/users/layout.tsx
+++ app/admin/users/layout.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth'
+import { GileadHeader } from '@/components/gilead-header'
 import { Header } from '@/components/Header'
 interface ChatLayoutProps {
   children: React.ReactNode
@@ -8,7 +9,7 @@ export default async function ChatLayout({ children }: ChatLayoutProps) {
   const session = await auth()
   return (
     <>
-      <Header />
+      <GileadHeader />
       <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden w-full">
         {children}
       </div>
